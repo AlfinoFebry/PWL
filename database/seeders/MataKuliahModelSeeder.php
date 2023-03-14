43fc4765d0f4036230ba12e493b03ccd6a603869
AlfinoFebryKrissaputra <alfinofebry03@gmail.com>
2023-03-14 22:12:33 +0700

    Seed mata kuliah

--- database/seeders/MataKuliahModelSeeder.php
+++ database/seeders/MataKuliahModelSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MataKuliahModelSeeder extends Seeder
 {
@@ -14,6 +15,25 @@ class MataKuliahModelSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('mata_kuliah')->insert([
+            ['nama_matkul' => 'Proyek 1',
+            'hari' => 'Senin'],
+            ['nama_matkul' => 'Manajemen Proyek',
+            'hari' => 'Selasa'],
+            ['nama_matkul' => 'Jaringan Komputer',
+            'hari' => 'selasa'],
+            ['nama_matkul' => 'Kewarganegaraan',
+            'hari' => 'Rabu'],
+            ['nama_matkul' => 'Web Lanjut',
+            'hari' => 'Rabu'],
+            ['nama_matkul' => 'Praktikum Jarkom',
+            'hari' => 'Kamis'],
+            ['nama_matkul' => 'Statistik',
+            'hari' => 'Kamis'],
+            ['nama_matkul' => 'Business Intelligence',
+            'hari' => 'Jumat'],
+            ['nama_matkul' => 'ADBO',
+            'hari' => 'Jumat']
+        ]);
     }
 }
