d6dfbbdf12c16013123bddc18bf1ac95e0bcb82b
AlfinoFebryKrissaputra <alfinofebry03@gmail.com>
2023-03-14 20:29:04 +0700

    seed hobi

--- app/Models/HobiModel.php
+++ app/Models/HobiModel.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class HobiModel extends Model
 {
-    use HasFactory;
+    protected $table = 'hobi';
+    protected $primaryKey = 'id_tabel';
 }
--- database/seeders/HobiModelSeeder.php
+++ database/seeders/HobiModelSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class HobiModelSeeder extends Seeder
 {
@@ -14,6 +15,10 @@ class HobiModelSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('hobi')->insert([
+            'nama_hobi' => 'Membaca',
+            'nama_hobi' => 'Mendengarkan Musik',
+            'nama_hobi' => 'Bernyanyi',
+        ]);
     }
 }
