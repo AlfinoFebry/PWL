161ff87bfd687dd23b6f91039ac98ea60509dbc8
AlfinoFebryKrissaputra <alfinofebry03@gmail.com>
2023-03-14 21:54:58 +0700

    seed keluarga

--- database/seeders/KeluargaModelSeeder.php
+++ database/seeders/KeluargaModelSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KeluargaModelSeeder extends Seeder
 {
@@ -14,6 +15,13 @@ class KeluargaModelSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('keluarga')->insert([
+            ['nama_keluarga' => 'Kristian Sapto Putro',
+            'status' => 'Ayah'],
+            ['nama_keluarga' => 'Satin',
+            'status' => 'Ibu'],
+            ['nama_keluarga' => 'Alfarizam Krissaputra',
+            'status' => 'Saudara']
+        ]);
     }
 }
